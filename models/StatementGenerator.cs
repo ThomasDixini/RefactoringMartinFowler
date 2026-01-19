484a85abceec05b001f3b4b5e4fefdd2ac182f8a
ThomasDixini <thomasdixini.dev@gmail.com>
2026-01-19 19:21:26 -0300

    refactor: :recycle: Extracting function Total Amount For

--- models/StatementGenerator.cs
+++ models/StatementGenerator.cs
@@ -6,11 +6,17 @@ namespace RefatoringMartinFowler.models
     public class StatementGenerator
     {
         public Dictionary<string, Play> Plays { get; set; } = new Dictionary<string, Play>();
+        public Invoice Invoice { get; set; }
         public StatementGenerator()
         {
             var playsJson = File.ReadAllText(@"C:\Projetos\RefatoringMartinFowler\jsons\Plays.json");
             var plays = JsonSerializer.Deserialize<Dictionary<string, Play>>(playsJson) ?? throw new Exception("Plays deserialization resulted in null");
+
+            var invoiceJson = File.ReadAllText(@"C:\Projetos\RefatoringMartinFowler\jsons\Invoices.json");
+            var invoices = JsonSerializer.Deserialize<List<Invoice>>(invoiceJson) ?? throw new Exception("Invoices deserialization resulted in null");
+
             Plays = plays;
+            Invoice = invoices.First();
         }
         public string Statement(Invoice invoice, Dictionary<string, Play> plays)
         {
@@ -23,12 +29,8 @@ namespace RefatoringMartinFowler.models
                 result += $"{PlayFor(perf).Name}: {Usd(AmountFor(perf))} ({perf.Audience} seats)\n";
                 totalAmount += AmountFor(perf);
             }
-            
-            decimal volumeCredits = 0m;
-            foreach(var perf in invoice.Performances)
-            {
-                volumeCredits += VolumeCreditsFor(perf);
-            }
+
+            decimal volumeCredits = TotalAmountFor();
 
             result += $"Amount owed is {Usd(totalAmount)}\n";
             result += $"You earned {volumeCredits} credits\n";
@@ -78,6 +80,16 @@ namespace RefatoringMartinFowler.models
         public string Usd(decimal amount)
         {
             return (amount / 100).ToString("C", CultureInfo.GetCultureInfo("en-US"));
+        }
+
+        public decimal TotalAmountFor()
+        {
+            decimal volumeCredits = 0m;
+            foreach(var perf in Invoice.Performances)
+            {
+                volumeCredits += VolumeCreditsFor(perf);
+            }
+            return volumeCredits;
         }
     }
 }
